ea9d6169218fe4c9f4b59d504c35ddbea7333cda
dominusac12 <dev.dominusac12@gmail.com>
2019-02-04 11:04:59 +0100

    Fixed DB bug

--- script/create-shelf.js
+++ script/create-shelf.js
@@ -2,7 +2,8 @@ const storage = require('electron-json-storage');
  
 storage.get('settings', function(error, data) {
   if (error) throw error;
-  var path = data.path + '\shelf.db'
+  var path = data.path + '/shelf.db'
+  console.log(path);
   var Datastore = require('nedb')
   , db = new Datastore({ filename: path, autoload: true });
   document.getElementById('submit').addEventListener('click', function(){
