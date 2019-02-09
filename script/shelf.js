748f1f738709ad64310f94598598ec46d1fd4eee
dominusac12 <dev.dominusac12@gmail.com>
2019-02-09 15:23:16 +0100

    Update shelf.js

--- script/shelf.js
+++ script/shelf.js
@@ -1,10 +1,7 @@
 const storage = require('electron-json-storage');
-
-var test_tr = 6;
-var test_th = 6;
-
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+var index = 0;
 drawTable(0);
 
 function drawTable(index){
@@ -15,12 +12,21 @@ function drawTable(index){
         //load Database
         var Datastore = require('nedb')
         , db = new Datastore({ filename: path, autoload: true });
+        
+        //clear table
+        document.getElementById('tbody').innerHTML = '';
+        document.getElementById('tr').innerHTML = '<th></th>';
+
         db.find({}, function(err, docs){
             if(index+1 == docs.length){
                 document.getElementById('next-btn').disabled = true;
+            }else{
+                document.getElementById('next-btn').disabled = false;
             }
-            if(index <= 0){
+            if(index == 0){
                 document.getElementById('previous-btn').disabled = true;
+            }else{
+                document.getElementById('previous-btn').disabled = false;
             }
             for(var i = 1; i < parseInt(docs[index].columns)+1; i++){
                 var html = '<tr>' + '<th>' + i + '</th>' +'</tr>';
@@ -34,3 +40,11 @@ function drawTable(index){
         });
     });
 }
+document.getElementById('next-btn').addEventListener("click", function(){
+    index++;
+    drawTable(index);
+});
+document.getElementById('previous-btn').addEventListener("click", function(){
+    index--;
+    drawTable(index);
+});
