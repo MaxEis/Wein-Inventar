f6c055ae0b19bf163899811a1a7d2c4910499bc0
dominusac12 <dev.dominusac12@gmail.com>
2019-02-09 21:01:51 +0100

    Update

--- script/shelf.js
+++ script/shelf.js
@@ -18,35 +18,38 @@ function drawTable(index){
         document.getElementById('tr').innerHTML = '<th></th>';
 
         db.find({}, function(err, docs){
-            document.getElementById('shelfName').innerHTML = docs[index].name;
-            if(index+1 == docs.length){
-                document.getElementById('next-btn').disabled = true;
+            if(docs.length == 0){
+                self.location.href = './first-setup.html';
             }else{
-                document.getElementById('next-btn').disabled = false;
-            }
-            if(index == 0){
-                document.getElementById('previous-btn').disabled = true;
-            }else{
-                document.getElementById('previous-btn').disabled = false;
-            }
-            for(var i = 1; i < parseInt(docs[index].columns)+1; i++){
-                var html = '<tr>' + '<th>' + i + '</th>' +'</tr>';
-                document.getElementById('tbody').innerHTML = document.getElementById("tbody").innerHTML + html; 
-            }
-            var letter;
-            var letterId = 0;
-            var sletter = 0;
-            for(var i = 0; i < parseInt(docs[index].rows); i++){
-                if(i > 25){
-                    letter = 'A' + alphabet.charAt(i-26);
-
+                document.getElementById('shelfName').innerHTML = docs[index].name;
+                if(index+1 == docs.length){
+                    document.getElementById('next-btn').disabled = true;
                 }else{
-                    letter = alphabet.charAt(i);
+                    document.getElementById('next-btn').disabled = false;
                 }
-                var html = '<th>' + letter + '</th>';
-                document.getElementById('tr').innerHTML = document.getElementById("tr").innerHTML + html; 
-            }
+                if(index == 0){
+                    document.getElementById('previous-btn').disabled = true;
+                }else{
+                    document.getElementById('previous-btn').disabled = false;
+                }
+                for(var i = 1; i < parseInt(docs[index].columns)+1; i++){
+                    var html = '<tr>' + '<th>' + i + '</th>' +'</tr>';
+                    document.getElementById('tbody').innerHTML = document.getElementById("tbody").innerHTML + html; 
+                }
+                var letter;
+                var letterId = 0;
+                var sletter = 0;
+                for(var i = 0; i < parseInt(docs[index].rows); i++){
+                    if(i > 25){
+                        letter = 'A' + alphabet.charAt(i-26);
 
+                    }else{
+                        letter = alphabet.charAt(i);
+                    }
+                    var html = '<th>' + letter + '</th>';
+                    document.getElementById('tr').innerHTML = document.getElementById("tr").innerHTML + html; 
+                }
+            }
         });
     });
 }
